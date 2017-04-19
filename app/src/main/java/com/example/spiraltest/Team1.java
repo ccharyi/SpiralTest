924b75c67ecdfcb866d953328835ee7cd8ab1c09
Charles Wang <charlesysw@gmail.com>
2017-04-19 18:47:31 -0400

    does it work?

--- app/src/main/java/com/example/spiraltest/Team1.java
+++ app/src/main/java/com/example/spiraltest/Team1.java
@@ -11,5 +11,6 @@ public class Team1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team1);
+        ///???
     }
 }
